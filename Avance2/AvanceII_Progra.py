27689409f7615c5afab8898bdc306e579cc00dcd
Brayton Maccoy <braymaccoy@hotmail.com>
2023-08-01 01:10:02 -0600

    Avance Configuración avanzada y creacion de archivos

--- Avance2/AvanceII_Progra.py
+++ Avance2/AvanceII_Progra.py
@@ -1,20 +1,42 @@
 #Proyecto de Programación Básica
 
+import getpass
+import os
 
 
 #Creacion de Funciones
+#Funcion para el registro de usuario
+def registrar_usuario():
+    intentos_max = 3
+    monto_minimo =  1000
+    intentos = 0
+    while intentos < intentos_max:  #Estructura WHILE con estructura de desicion para identificar el numero de intentos
+        usuario_id = input("Ingrese un nombre de usuario o ID: ")   #Se le solicita al usuario el ID
+        if len(usuario_id) < 5:
+            print("El ID debe tener al menos cinco caracteres.")
+        elif usuario_id in usuarios_registrados:
+            print("El ID ingresado ya está registrado.")
+        else:
+            nombre = input("Ingrese su nombre: ")   #Solicitud de nombre
+            pin = getpass.getpass("Ingrese su PIN (mínimo 6 dígitos): ")   #Solicitar al usuario el pin y utilizar la biblioteca "getpass"
+            if len(pin) < 6:
+                print("El PIN debe tener al menos seis dígitos.")
+            else:
+                deposito = obtener_deposito_equivalente()
+                if deposito >= monto_minimo:
+                    usuarios_registrados[usuario_id] = {
+                        "nombre": nombre,
+                        "pin": pin,
+                        "saldo": deposito
+                    }
+                    print(f"Registro exitoso. ¡Bienvenido(a) {nombre} al DreamWorld Casino!")
+                    return
+                else:
+                    print("El monto depositado es inferior al mínimo requerido. Intente nuevamente.")
+        intentos += 1
+        print(f"Tiene {intentos_max - intentos} intentos restantes.")
 
-def registro_usuario():
-    print ("Prueba 1")
-
-#1.	Registro de usuario
-#a.	Si el Usuario elige la función "Registro de usuario nuevo" : Se deben cumplir las siguientes condiciones para registrar el nombre de usuario:
-#b.	El nombre usuario debe ser mayor o igual a 5 caracteres: 
-#c.	Si el N.U es menor a 5 caracteres se imprime un error y el mensaje "El Nombre de usuario debe ser mayor a 5 caracteres". 
-#d.	El nombre de usuario no debe estar previamente registrado (Estructura del Sistema de Archivos y biblioteca os)
-#e.	Si se genera una alerta el usuario tiene 3 intentos para agregar la información correcta, al fallar los 3 intentos vuelve al menú inicio.
-
-
+    print("Se excedió el máximo de intentos para depositar el mínimo de dinero requerido, volviendo al menú principal.")
 #2.	Solicitar el nombre
 
 #3.	Creación de PIN:
@@ -125,29 +147,81 @@ def dreamWorldCasino():
 
 
 def configuracionAvanzada():
-    print ("Prueba 3")
+    pinConfgAvanzada = "1234"
+    pinIngresado = getpass.getpass("Ingrese el Pin Especial\n")             
+    if pinIngresado == pinConfgAvanzada:                               
+            print ("Configuración Avanzada\n")
+            print ("Menú\n")
+            print ("a. Eliminar Usuario\n")
+            print ("b. Modificar Valores\n")
+            print ("c. salir")
+            respuesta = input("Ingrese una opción\n")
+            if respuesta == "a":
+                borrarUsuario()
+
+            elif respuesta == "b":
+                valoresDelSistema()
+
+            elif respuesta == "c":
+                menu_Principal()
+
+            else:
+                print ("Opción Incorrecta")
+                menu_Principal()
+
+    else:                                  #2.	Si el pin ingresado es incorrecto se devuelve al menú principal
+        menu_Principal()
 
 
-#1.	Solicitud de PIN especial
-#2.	Si el pin ingresado es incorrecto se devuelve al menú principal
-#3.	El PIN especial no debe ser mostrado en la pantalla
-#4.	Si el PIN es correcto desplegar submenú
-#a.	Eliminar Usuario
-#b.	Modificar Valores
-#c.	Salir
+def borrarUsuario():
+    print("Testing")
 
 
+def valoresDelSistema():
+    print ("Bienvenido a los Valores de sistema/n")
+    print ("a. Tipo de cambio: Colones a Dolares/n")
+    print ("b. Tipo de cambio: Bitcoin a Dolares/n")
+    print ("c.	Valor acumulado Tragamonedas/n")
+    print ("d.	Apuesta mínima Tragamonedas/n")
+    print ("e.	Apuesta mínima Blackjack/n")
+    print ("f.	Inversión mínima para registrarse/n")
+    print ("g.	Salir/n")
+    opcion = input("Seleccione la opción que quiere modificar\n")
 
+    if opcion == "a":
+        print ("a. Tipo de cambio: Colones a Dolares/n")
+        #Crear archivo con tipo de cambio y poder cambiarlo
+    elif opcion == "b":
+        print ("b. Tipo de cambio: Bitcoin a Dolares/n")
+        #Crear archivo con tipo de cambio y poder cambiarlo
+    elif opcion == "c":
+        print ("c.	Valor acumulado Tragamonedas/n")
 
-#A)	Eliminar usuario
+    elif opcion == "d":
+        print ("d.	Apuesta mínima Tragamonedas/n")
+
+    elif opcion == "e":
+        print ("e.	Apuesta mínima Blackjack/n")
+
+    elif opcion == "f":
+        deposito = str(input("Ingrese el monto minimo del Deposito\n"))
+        fileDeposit = open("deposito.txt", "w")
+        fileDeposit.write(deposito)
+        print ("El monto del deposto fué cambiado a {} correctamente".format deposito)
+        fileDeposit.close()
+
+    elif opcion == "g":
+        menu_Principal
+
+    else: 
+        print("Opción seleccionada no es correcta")
+        valoresDelSistema()
+
 
 #1.	Solicitar ID del usuario que se va a eliminar
 #2.	Si el usuario existe se eliminan todos los registros asociados a ese usuario
 #3.	Si no existe se muestra mensaje de error
 #4.	Se devuelve al menú principal
-
-
-
 
 #B)	Modificar Valores del Sistema 
 #1.	Crear submenú
@@ -173,34 +247,35 @@ def salir():
 
 #---------------------------------------Programa Principal--------------------------------#
 
-menuPrincipal = input("Seleccione una opción\na.Registro de usuario nuevo\nb.DreamWorld Casino\nc.Configuración avanzada\nd.Salir\nSu respuesta:  ")
-
-while True:
+def menu_Principal():
     
-    if menuPrincipal == "a":
-        registro_usuario()
-        break
-    
-    elif menuPrincipal == "b":
-        dreamWorldCasino()
-        break
+    menuPrincipal = input("Seleccione una opción\na.Registro de usuario nuevo\nb.DreamWorld Casino\nc.Configuración avanzada\nd.Salir\nSu respuesta:  ")
+
+    while True:
+        
+        if menuPrincipal == "a":
+            registrar_usuario()
+            break
+        
+        elif menuPrincipal == "b":
+            dreamWorldCasino()
+            break
 
 
-    elif menuPrincipal == "c":
-        configuracionAvanzada()
-        break
-    
+        elif menuPrincipal == "c":
+            configuracionAvanzada()
+            break
+        
 
-    elif menuPrincipal == "d":
-        print ("Gracias por visitar DreamWorld Casino")
-        break
-    
-    else:
-        print ("Opción no valida")
-        break
+        elif menuPrincipal == "d":
+            print ("Gracias por visitar DreamWorld Casino")
+            break
+        
+        else:
+            print ("Opción no valida")
+            break
 
-
-
+print (menu_Principal())
     
 
 
